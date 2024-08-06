b3befb878d0383d4a1be1e92e7a305d65fa5fb52
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2024-08-06 14:03:59 +0330

    first-commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App.tsx";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./loginForm/LoginForm.tsx";
+import RequestPage from "./requestPage/RequestPage.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="requestForm/" element={<App />} />
+        <Route path="requestForm/login" element={<LoginForm />} />
+        <Route path="requestForm/requests" element={<RequestPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
