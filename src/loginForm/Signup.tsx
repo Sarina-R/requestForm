3fa4aa4dd76b9659d051acd809356148d0acb7e2
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2024-08-14 12:46:55 +0330

    signup validation detail

--- src/loginForm/Signup.tsx
+++ src/loginForm/Signup.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   MDBBtn,
@@ -13,6 +14,7 @@ interface SigninProps {
   setPwd: React.Dispatch<React.SetStateAction<string>>;
   setEmail: React.Dispatch<React.SetStateAction<string>>;
   setConfirmPwd: React.Dispatch<React.SetStateAction<string>>;
+  pwd: string;
 }
 
 const Signup: React.FC<SigninProps> = ({
@@ -21,7 +23,65 @@ const Signup: React.FC<SigninProps> = ({
   setPwd,
   setConfirmPwd,
   handleSubmit,
+  pwd,
 }) => {
+  const [emailClass, setEmailClass] = useState<string>("");
+  const [userNameClass, setUserNameClass] = useState<string>("");
+  const [pwdClass, setPwdClass] = useState<string>("");
+  const [pwdConfirmClass, setPwdConfirmClass] = useState<string>("");
+
+  const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setEmail(value);
+    if (!validateEmail(value)) {
+      setEmailClass("is-invalid");
+    } else {
+      setEmailClass("is-valid");
+    }
+  };
+
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUserName(value);
+    if (value.length < 4 || value.length > 20) {
+      setUserNameClass("is-invalid");
+    } else {
+      setUserNameClass("is-valid");
+    }
+  };
+
+  const handlePwdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    setPwd(value);
+    if (!re.test(value)) {
+      setPwdClass("is-invalid");
+    } else {
+      setPwdClass("is-valid");
+    }
+  };
+
+  const handlePwdConfirmChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setConfirmPwd(value);
+    if (value !== pwd || value.length < 6) {
+      setPwdConfirmClass("is-invalid");
+    } else {
+      setPwdConfirmClass("is-valid");
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSubmit(event as unknown as React.FormEvent);
+    }
+  };
+
   return (
     <MDBContainer fluid>
       <MDBRow>
@@ -34,48 +94,98 @@ const Signup: React.FC<SigninProps> = ({
               Sign up
             </h3>
 
-            <MDBInput
-              wrapperClass="mb-4 mx-5 w-100"
-              label="Email address"
-              id="formControlSi1"
-              type="email"
-              size="lg"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4 mx-5 w-100"
-              label="UserName"
-              id="formControlSi2"
-              type="username"
-              size="lg"
-              onChange={(e) => setUserName(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4 mx-5 w-100"
-              label="Password"
-              id="formControlSi3"
-              type="password"
-              size="lg"
-              onChange={(e) => setPwd(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4 mx-5 w-100"
-              label="Confirm Password"
-              id="formControlSi4"
-              type="password"
-              size="lg"
-              onChange={(e) => setConfirmPwd(e.target.value)}
-            />
+            <form onSubmit={handleSubmit}>
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Email address"
+                id="formControlSi1"
+                type="email"
+                size="lg"
+                onChange={handleEmailChange}
+                onKeyDown={handleKeyDown}
+                className={emailClass}
+              />
+              {emailClass === "is-invalid" ? (
+                <p className="text-danger mx-5 w-100 pl-5">
+                  User Name must be more than
+                </p>
+              ) : emailClass === "is-valid" ? (
+                <></>
+              ) : null}
 
-            <MDBBtn
-              type="button"
-              className="mb-4 px-5 mx-5 w-100"
-              color="info"
-              size="lg"
-              onClick={handleSubmit}
-            >
-              Sign up
-            </MDBBtn>
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Username"
+                id="formControlSi2"
+                type="text"
+                size="lg"
+                onChange={handleUserNameChange}
+                onKeyDown={handleKeyDown}
+                className={userNameClass}
+              />
+              {userNameClass === "is-invalid" ? (
+                <p className="text-danger mx-5 w-100">
+                  Email must be in the format: example@domain.com.
+                </p>
+              ) : userNameClass === "is-valid" ? (
+                <></>
+              ) : null}
+
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100 "
+                label="Password"
+                id="formControlSi3"
+                type="password"
+                size="lg"
+                onChange={handlePwdChange}
+                onKeyDown={handleKeyDown}
+                className={pwdClass}
+              />
+              {pwdClass === "is-invalid" ? (
+                <p className="text-danger mx-5 w-100">
+                  Password must be at least 8 characters long and include at
+                  least one uppercase letter, one lowercase letter, one number,
+                  and one special character
+                </p>
+              ) : pwdClass === "is-valid" ? (
+                <></>
+              ) : null}
+
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Confirm Password"
+                id="formControlSi4"
+                type="password"
+                size="lg"
+                onChange={handlePwdConfirmChange}
+                onKeyDown={handleKeyDown}
+                className={pwdConfirmClass}
+              />
+              {pwdConfirmClass === "is-invalid" ? (
+                <p className="text-danger mx-5 w-100">
+                  Please confirm your password by entering it again
+                </p>
+              ) : pwdConfirmClass === "is-valid" ? (
+                <></>
+              ) : null}
+
+              <MDBBtn
+                type="submit"
+                className="mb-4 px-5 mx-5 w-100"
+                color="info"
+                size="lg"
+                disabled={
+                  pwdConfirmClass === "is-invalid" ||
+                  pwdClass === "is-invalid" ||
+                  emailClass === "is-invalid" ||
+                  userNameClass === "is-invalid"
+                    ? true
+                    : false
+                }
+              >
+                Sign up
+              </MDBBtn>
+            </form>
             <p className="small mb-5 pb-lg-3 ms-5">
               <a className="text-muted" href="#!">
                 Forgot password?
--- src/loginForm/SignupForm.tsx
+++ src/loginForm/SignupForm.tsx
@@ -14,13 +14,36 @@ const SignupForm = () => {
   const [userName, setUserName] = useState("");
   const [pwd, setPwd] = useState("");
   const [confirmPwd, setConfirmPwd] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    const validateEmail = (email: string) => {
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(email);
+    };
+
+    if (!validateEmail(email)) {
+      setError("Invalid email format");
+      return;
+    }
+
     if (pwd !== confirmPwd) {
-      alert("Passwords do not match");
+      setError("Passwords do not match");
+      return;
+    }
+
+    const existingUser = users.find((user) => user.userName === userName);
+    if (existingUser) {
+      setError("Username already taken");
+      return;
+    }
+
+    const existingEmail = users.find((user) => user.email === email);
+    if (existingEmail) {
+      setError("Email already taken");
       return;
     }
 
@@ -41,17 +64,22 @@ const SignupForm = () => {
       navigate(`/requestForm/user/${newUser.id}`);
     } catch (error) {
       console.error("Error creating user:", error);
+      setError("Error creating user");
     }
   };
   return (
     <>
       <Header />
+      {error && (
+        <div style={{ color: "red", textAlign: "center" }}>{error}</div>
+      )}
       <Signup
         handleSubmit={handleSubmit}
         setEmail={setEmail}
         setUserName={setUserName}
         setPwd={setPwd}
         setConfirmPwd={setConfirmPwd}
+        pwd={pwd}
       />
     </>
   );
